c55fe6a3a351d9d6724cedc60c21d168d13e625c
Jeroen Goossens <jeroen.goossens@ucll.Be>
2025-02-20 20:18:50 +0100

    game loop

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,7 @@ import copy
 import random
 import pygame
 
+pygame.init()
 # game variables
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 one_deck = 4 * cards
@@ -11,10 +12,51 @@ game_deck = copy.deepcopy(decks * one_deck) # make sure only copy gets modified 
 # setting up pygame-window
 WIDTH = 600
 HEIGHT = 900
-pygame.display.set_mode([WIDTH, HEIGHT])
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Blackjack 21')
 
 # game variables
 fps = 60
-pygame.time.Clock()
+timer = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 44)
+active = True
+
+#declare game conditions and buttons:
+def draw_game(act):
+    button_list = []
+    #initially on startup - only option is deal new hand
+    if not act:
+        deal = pygame.draw.rect(screen, 'white', [150, 20, 300, 100], 0, 5)
+        pygame.draw.rect(screen, 'green', [150, 20, 300, 100], 3, 5)
+        deal_text = font.render('DEAL HAND', True, 'black')
+        screen.blit(deal_text, (165, 50))
+        button_list.append(deal)
+    # once game started, put hit and stand buttons + win/loss records
+    else:
+        hit = pygame.draw.rect(screen, 'white', [0, 700, 300, 100], 0, 5)
+        pygame.draw.rect(screen, 'green', [0, 700, 300, 100], 3, 5)
+        hit_text = font.render('HIT ME', True, 'black')
+        screen.blit(hit_text, (55, 735))
+        button_list.append(hit)
+
+        stand = pygame.draw.rect(screen, 'white', [300, 700, 300, 100], 0, 5)
+        pygame.draw.rect(screen, 'green', [300, 700, 300, 100], 3, 5)
+        stand_text = font.render('STAND', True, 'black')
+        screen.blit(stand_text, (355, 735))
+        button_list.append(stand)
+
+# main game loop
+run = True
+while run:
+    # run game at framerate and fill screen with background color
+    timer.tick(fps)
+    screen.fill('black')
+    buttons = draw_game(active)
+
+    # event handling, if quit pressed, exit game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.flip()
+pygame.quit()
