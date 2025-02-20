6cc7c01c779848cc24aac79a55fb584de3d19c16
Jeroen Goossens <jeroen.goossens@ucll.Be>
2025-02-20 19:32:49 +0100

    setting up game variables

--- blackjack.py
+++ blackjack.py
@@ -5,5 +5,16 @@ import pygame
 # game variables
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 one_deck = 4 * cards
-decks = 4 # prevent cardcounting
+decks = 1 # prevent cardcounting
 game_deck = copy.deepcopy(decks * one_deck) # make sure only copy gets modified and not original cards list
+
+# setting up pygame-window
+WIDTH = 600
+HEIGHT = 900
+pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Blackjack 21')
+
+# game variables
+fps = 60
+pygame.time.Clock()
+font = pygame.font.Font('freesansbold.ttf', 44)
