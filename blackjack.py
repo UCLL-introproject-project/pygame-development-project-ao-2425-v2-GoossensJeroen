f4cb1ec8f8d6baf36f9c2816dd0dfb0f10f3076f
Jeroen Goossens <jeroen.goossens@ucll.Be>
2025-02-21 17:21:45 +0100

    calculate_score and draw_score functions

--- blackjack.py
+++ blackjack.py
@@ -31,14 +31,64 @@ game_deck = copy.deepcopy(decks * one_deck) # make sure only copy gets modified 
 my_hand = []
 dealer_hand = []
 outcome = 0
+reveal_dealer = True
 
 # deal cards randomly from deck, one at a time
 def deal_cards(current_hand, current_deck):
     card = random.randint(0, len(current_deck))
     current_hand.append(current_deck[card-1])
     current_deck.pop(card-1)
-    print(current_hand, current_deck)
     return current_hand, current_deck
+
+# draw scores on screen
+def draw_scores(player, dealer):
+    screen.blit(font.render(f'Score [{player}]', True, 'white'), (350, 400))
+    if reveal_dealer:
+        screen.blit(font.render(f'Score [{dealer}]', True, 'white'), (350, 100))
+
+# draw cards visually on screen
+def draw_cards(player, dealer, reveal):
+    for i in range (len(player)):
+        pygame.draw.rect(screen, 'white', [70 + (70 * i), 460 + (5 * i), 120, 220], 0, 5)
+        screen.blit(font.render(player[i], True, 'black'),(75 + 70 * i, 465 + 5 * 1 )) 
+        screen.blit(font.render(player[i], True, 'black'),(75 + 70 * i, 635 + 5 * 1 )) 
+        pygame.draw.rect(screen, 'red', [70 + (70 * i), 460 + (5 * i), 120, 220], 5, 5)
+        
+    # if player hasn't finished turn, dealer hides one card
+    for i in range (len(dealer)):
+        pygame.draw.rect(screen, 'white', [70 + (70 * i), 160 + (5 * i), 120, 220], 0, 5)
+        if i != 0 or reveal:
+            screen.blit(font.render(dealer[i], True, 'black'),(75 + 70 * i, 165 + 5 * 1 )) 
+            screen.blit(font.render(dealer[i], True, 'black'),(75 + 70 * i, 335 + 5 * 1 ))
+        else:
+            screen.blit(font.render('???', True, 'black') ,(75 + 70 * i, 165 + 5 * 1 )) 
+            screen.blit(font.render('???', True, 'black') ,(75 + 70 * i, 335 + 5 * 1 ))
+             
+        pygame.draw.rect(screen, 'blue', [70 + (70 * i), 160 + (5 * i), 120, 220], 5, 5)
+
+# pass in hand and get best possible score
+def calculate_score(hand):
+    #calculate new score evnery time and check how many aces
+    hand_score = 0
+    aces_count = hand.count('A')
+    # for loop check wwat highest possible score is
+    for i in range (len(hand)):
+        # for 2-9 just add number (int) to total
+        for j in range(8):
+            if hand[i] == cards[j]:
+                hand_score += int(hand[i])
+        # for 10 and face cards add 10
+        if hand[i] in ['10', 'J', 'Q', 'K']:
+            hand_score += 10
+        # for aces start by adding 11, check to reduce afterwards
+        elif hand[i] == 'A':
+            hand_score += 11
+        # determine how many aces need to be 1 instead of 11 to be under 21
+    if hand_score > 21 and aces_count > 0:
+        for i in range(aces_count):
+            if hand_score > 21:
+                hand_score -= 10
+    return hand_score
 
 #declare game conditions and buttons:
 def draw_game(act, records):
@@ -81,11 +131,17 @@ while run:
         for i in range(2):
             my_hand, game_deck = deal_cards(my_hand, game_deck)
             dealer_hand, game_deck = deal_cards(dealer_hand, game_deck)
-        print(my_hand, dealer_hand)
     initial_deal = False
 
     # once game if activated and dealt, calculate scores and display cards
-
+    if active:
+        player_score = calculate_score(my_hand)
+        draw_cards(my_hand, dealer_hand, reveal_dealer)
+        if reveal_dealer:
+            dealer_score = calculate_score(dealer_hand)
+            if dealer_score < 17:
+                dealer_hand, game_deck = deal_cards(dealer_hand, game_deck)
+        draw_scores(player_score, dealer_score)
 
     buttons = draw_game(active, records)
 
